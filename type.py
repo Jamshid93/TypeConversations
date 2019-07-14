1b623488f1c3b3f32bbc7a0f1240613381ab7697
Jamshid93 <hamzayevjf@gmail.com>
2019-07-14 17:49:35 +0500

    Type conversation

---
+++ type.py
@@ -0,0 +1,9 @@
+name=input("What is your name?")
+print(type(name))
+birth_day=input("Pleace tell me your brithday?")
+print(type(birth_day))
+age=2019-int(birth_day)
+favourite_color=input("What is your favourite color?")
+print(type(favourite_color))
+print(name +" born at the "+ birth_day  +" nowadays his age is " +str(age) + " his likes "+ favourite_color)
+# If we concatenate " int "and " str" we write in a print no int only str all of str or all of int mix is not.
